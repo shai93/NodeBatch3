1d4033463153e3cd8e834e782bf6e7d37bfc5340
shailesh salekar <shailesh.salekar@accionlabs.com>
2020-12-20 20:41:21 +0530

    Login with google, passport

--- Express snippet/app.js
+++ Express snippet/app.js
@@ -6,13 +6,30 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var photoRouter =  require('./routes/photos');
+var photoRouter = require('./routes/photos');
+var successRouter = require('./routes/success');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var app = express();
 
+
+passport.use(new GoogleStrategy({
+  clientID: "39841039586-ko6cmnd2l8psnqmrrcgrak56f9lueltu.apps.googleusercontent.com",
+  clientSecret: "flM4UabFV6mnaxSCb-Wdeo96",
+  callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    return done("", profile)
+    //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //    return done(err, user);
+    //  });
+  }
+));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,15 +39,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/photos', photoRouter)
+app.use('/photos', photoRouter);
+app.use('/success', successRouter);
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }, function (err) {
+  console.log('esssss ', err)
+}), function (req, res) {
+  console.log(res)
+  // res.redirect('/');
+});
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
